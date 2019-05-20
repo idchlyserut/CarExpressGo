80887c630325dda89826c98fa62286c5837229af
Amirah Syafiqah <47800433+minNt27@users.noreply.github.com>
2019-05-21 00:58:03 +0800

    Add files via upload

--- book.php
+++ book.php
@@ -2,6 +2,7 @@
 //including the database connection file
 include('..\Cardoor--master\config.php');
 
+
 if(isset($_POST["bookCar"])) {	
 	
 	$location = mysqli_real_escape_string($mysqli, $_POST['location']);
@@ -10,22 +11,18 @@ if(isset($_POST["bookCar"])) {
 	$car_type = mysqli_real_escape_string($mysqli, $_POST['car_type']);
 	
 	
-	//insert data to database
-	//$bookCar=$_POST["bookCar"];
+	$pickdate = date('Y-m-d', strtotime($_POST['pick_date']));
+	$returndate = date('Y-m-d', strtotime($_POST['pick_date']));
+	
 	$sql=mysqli_query($mysqli, "INSERT INTO bookinginfo(location,pick_date,return_date,car_type) 
-	VALUES('$_POST[location]','$_POST[pick_date]','$_POST[return_date]','$_POST[car_type]')");
+	VALUES('$_POST[location]','$pickdate','$returndate','$_POST[car_type]')");
+	
 	
 	echo "<font color='green'>Car successfully booked.";
-		
-	/*	$result = mysqli_query($mysqli, "INSERT INTO bookinginfo(location,pick_date,return_date,car_type) VALUES('$location','$pick_date','$return_date','$car_type')");
-		
-		//display success message
-		if($mysqli->query($location, $pick_date, $return_date, $car_type) === true){
-			
-			//echo "<br/><a href='index3.php'>View Result</a>";
-		} else {
-			echo "ERROR: Could not able to book car." . $mysqli->error;
-	*/
+	
+	
+	header('Refresh: 0;index3.php');
+
 }
 
 ?>
